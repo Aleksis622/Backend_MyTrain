8477df576cfde27d3e72ddecb55b2b8e2246c5b4
Aleksis622 <ipb23.a.eglitis@vtdt.edu.lv>
2026-09-10 11:19:11 +0300

    fixed api so it sends finally train list data to my react project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,25 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
+
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use([
+            HandleCors::class,
+        ]);
+    })
+
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         api: __DIR__.'/../routes/api.php',
-        health: '/up',
+        commands: __DIR__.'/../routes/console.php',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
-    })->create();
+    ->create();
